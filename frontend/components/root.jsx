25b93e414a5b41e305720a8f5ed3c2e2040427c4
dankim93 <dan_kim93@hotmail.com>
2017-01-11 10:07:09 -0800

    Add onEnter hook

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -7,15 +7,27 @@ import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
 
-const Root = ({ store }) => (
-  <Provider store={store}>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App }>
-        <Route path="/login" component={SessionFormContainer}  />
-        <Route path="/signup" component={SessionFormContainer}  />
-      </Route>
-    </Router>
-  </Provider>
-);
+
+
+const Root = ({ store }) => {
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return(
+    <Provider store={store}>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App }>
+          <Route path="/login" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
+          <Route path="/signup" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
+        </Route>
+      </Router>
+    </Provider>
+  );
+};
 
 export default Root;

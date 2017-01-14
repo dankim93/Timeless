5de63dc7aa6049939ccffbe8371a837d2636d876
dankim93 <dan_kim93@hotmail.com>
2017-01-13 16:57:33 -0800

    Setup figaro

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 User.create(username: 'guest', password: 'password')
 User.create(username: 'dankim', password: 'password')
 
-Photo.create(image_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg', title: 'berkeley', description: 'Nice view', user_id: 1, thumb_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg');
+Photo.create(image_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg', title: 'berkeley', description: 'San Francisco view from Berkeley', user_id: 1, thumb_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg');
 Photo.create(image_url: 'http://res.cloudinary.com/dyjarpgbt/image/upload/v1484332442/pexels-photo-287240_kfac3f.jpg', title: 'Vacation', description: 'Hiking during summer', user_id: 1, thumb_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg');
 Photo.create(image_url: 'http://res.cloudinary.com/dyjarpgbt/image/upload/v1484332576/sea-mountains-ocean-rocks_ahvnqa.jpg', title: 'SF', description: 'Golden Gate Bridge', user_id: 2, thumb_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg');
 Photo.create(image_url: 'http://res.cloudinary.com/dyjarpgbt/image/upload/v1484332438/pexels-photo-200303_jkgqpy.jpg', title: 'Dual screen', description: 'Working with multi-screens', user_id: 1, thumb_url: 'http://www.visitberkeley.com/images/header-rotation/OrangeSky_full_web.jpg');

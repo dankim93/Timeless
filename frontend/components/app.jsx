675d6c74cdc405848f4cee5a79ed66e9942bfda6
dankim93 <dan_kim93@hotmail.com>
2017-01-20 00:43:17 -0800

    Add footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavbarContainer from './navbar/navbar_container';
+import Footer from './splash/footer';
 import { Link } from 'react-router';
 
 const App = ({ children }) => (
   <div className='app'>
     <NavbarContainer />
     { children }
+    <Footer />
   </div>
 );
 
--- frontend/components/splash/footer.jsx
+++ frontend/components/splash/footer.jsx
@@ -4,11 +4,14 @@ class Footer extends React.Component {
 
   render(){
     return(
-      <div>
-        <img
-        <img
-      </div>
-
+      <section className='footer'>
+        <a href='https://github.com/dankim93'>
+          <img src='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png' />
+        </a>
+        <a href='https://www.linkedin.com/in/dankim93'>
+          <img className= 'linkedin' src='https://lh3.googleusercontent.com/00APBMVQh3yraN704gKCeM63KzeQ-zHUi5wK6E9TjRQ26McyqYBt-zy__4i8GXDAfeys=w300' />
+        </a>
+      </section>
     );
   }
 }

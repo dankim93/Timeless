a3f6c13465137dce93971f3db99fe586c5982277
dankim93 <dan_kim93@hotmail.com>
2017-05-22 17:52:41 -0700

    Add rspec tests for user model credentials and resetting session token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe User do
-    it { should validate_presence_of(:username) }
-    it "validates uniqueness of username" do
-      User.new(:password_digest => 'whatever', :username => 'something').save!(:validate => false)
-      should validate_uniqueness_of :username
-    end
-    # it { should ensure_inclusion_of(:username).in_array(["Cavaliers", "Suns", "Mavericks"])}
-  end
+  # describe User do
+  #   it { should validate_presence_of(:username) }
+  #   it "validates uniqueness of username" do
+  #     User.new(:password_digest => 'test', :username => 'testing').save!(:validate => false)
+  #     should validate_uniqueness_of :username
+  #   end
+  # end
+  let! (:user) { User.create!(username: 'guest', password: 'password')}
 
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_uniqueness_of(:username) }
+  end
+  
   describe "associations" do
     it { should have_many(:photos)}
     it { should have_many(:albums)}
     it { should have_many(:comments)}
   end
 
-  describe 'validations' do
-    before(:each) do
-      User.create!(username: 'test', password: 'password')
+  describe 'model_methods' do
+    describe '.find_by_credentials' do
+      context 'when given correct credentials' do
+        it 'should find the right user' do
+          correct = User.find_by_credentials('guest', 'password')
+          expect(user).to eq(correct)
+        end
+      end
+
+      context 'when given incorrect credentials' do
+        it 'should return nil' do
+          incorrect = User.find_by_credentials('guest', 'drowssap')
+          expect(incorrect).to eq(nil)
+        end
+      end
     end
-    it { should validate_presence_of(:username) }
-    it { should validate_presence_of(:password_digest) }
-    it { should validate_presence_of(:session_token) }
-    it { should validate_uniqueness_of(:username) }
+  end
+
+  describe 'instance_methods' do
+    describe '#reset_session_token!' do
+      it 'resets the session token' do
+        old_token = user.session_token
+        new_token = user.reset_session_token!
+        expect(old_token).not_to eq(new_token)
+      end
+    end
   end
 end
